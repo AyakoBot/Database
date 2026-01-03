3f01c22f06c03b5a57ab809029175230ddf5e30e
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2026-01-03 19:54:57 +0100

    Add generator script and update package.json scripts

---
+++ generator.ts
@@ -0,0 +1,23 @@
+import fs from 'fs/promises';
+
+import { glob } from 'glob';
+
+const paths = await glob('../Service/**/*.prisma');
+
+const dynContent = (await Promise.all(paths.map((path) => fs.readFile(path, 'utf-8'))))
+ .map((content) => content.split('//#endregion')[1].trim())
+ .join('\n\n');
+
+const [baseContent] = (await fs.readFile('./prisma/schema.prisma', 'utf-8')).split(
+ '//#region Models',
+);
+
+const finishedContent = `${baseContent.trim()}
+
+//#region Models
+
+${dynContent.trim()}
+
+//#endregion`;
+
+fs.writeFile('./prisma/schema.prisma', finishedContent);
