4890c0cc1aa89cbf17f0a0d2302439181df92c0a
Larsundso <Larsundso3@gmail.com>
2026-01-06 01:40:01 +0100

    Fix glob pattern in generator script to include 'src' directory for Prisma files

--- generator.ts
+++ generator.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 
 import { glob } from 'glob';
 
-const paths = await glob('../Service/**/*.prisma');
+const paths = await glob('../Service/src/**/*.prisma');
 
 const dynContent = (await Promise.all(paths.map((path) => fs.readFile(path, 'utf-8'))))
  .map((content) => content.split('//#endregion')[1].trim())
